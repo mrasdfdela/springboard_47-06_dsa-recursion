5adaed789628db409142f24a40da0ee2a6cda31c
Jerry Hsu <Jerry>
2021-10-13 19:02:19 -0400

    exercuses product & longest

--- recursion.js
+++ recursion.js
@@ -1,13 +1,17 @@
 /** product: calculate the product of an array of numbers. */
 
 function product(nums) {
-
+  if (nums.length===1) return nums[0];
+  return nums[0] * product( nums.slice(1) );
 }
 
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-
+  if (words.length===1) return words[0].length;
+  const wordLength = words[0].length;
+  let otherLength = longest(words.slice(1));
+  return wordLength > otherLength ? wordLength : otherLength;
 }
 
 /** everyOther: return a string with every other letter. */
